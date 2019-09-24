1163929d341d996161ce2b912d244ef2dfb9c249
learnserd <40648282+learnserd@users.noreply.github.com>
2019-09-24 10:58:35 +0800

    Update example_3D_seismic_data_reconstruction.m

--- example_3D_seismic_data_reconstruction.m
+++ example_3D_seismic_data_reconstruction.m
@@ -1,5 +1,6 @@
 % Demonstration script for 3D seismic reconstruction via Fast Iterative Adaptive Approach 
 % Corresponding paper:Fast Iterative Adaptive Approach for 3D Seismic Data Reconstruction
+% Decription: This demo tests FIAA on a small data without windowing technique.
 % Author: ZhiGang Dai
 % Email:  daizg@cug.edu.cn
 % Date: May, 8, 2018
@@ -8,21 +9,21 @@ clc;
 clear;
 close all;
 
-load 'prestack_shot9.mat';                                                                          %load data
+load 'prestack_shot9.mat';                %load data
 seis_x=Data(1:64,1:40,1:40);                                                    
-seis_origin=(seis_x-min(seis_x(:)))/(max(seis_x(:)-min(seis_x(:))));               %normalization
+seis_origin=(seis_x-min(seis_x(:)))/(max(seis_x(:)-min(seis_x(:))));   %normalization
 [nt,nx,ny]=size(seis_origin);
-m=0.5;                                                                                                       %missing rate            
-iter=10;                                                                                                      %Iteration number
+m=0.5;                                    %missing rate            
+iter=10;                                   %Iteration number
  m=round(nx*ny*m);
  L1=sort(randsample(nx*ny,m),'ascend'); 
  seis_sam=seis_origin;
  seis_sam(:,L1)=0;
- se_fft=fft(seis_sam);                                                                                  %transform data into frequency domain
-L2=find(seis_sam(1,:,:)~=0);                                                                       %Index of available data
-Index=get_Index(nx,ny);                                                                            %Index is used in get_E.m script
+ se_fft=fft(seis_sam);              %transform data into frequency domain
+L2=find(seis_sam(1,:,:)~=0);        %Index of available data
+Index=get_Index(nx,ny);             %Index is used in get_E.m script
 Df=ones(nx,ny,nt);                                                                              
-K=80;                                                                                                         %frequency grids: Kx=Ky=80
+K=80;                     %frequency grids: Kx=Ky=80
 tic
 for i=1:nt/2+1;                                                                                           
 i
@@ -33,9 +34,9 @@ toc
 MR=zeros(nt,nx,ny);
   MR(1:nt/2+1,:,:)=permute(Df(:,:,1:nt/2+1),[3,1,2]);
 MR(nt/2+2:nt,:,:)=conj(flipud(MR(2:nt/2,:,:)));
- Dt=real(ifft(MR));                                                                                 %transform the data back to time domain
- snr(a)= SNR1(seis_origin(:),Dt(:))                                                           %compute SNR
- Threedimension(seis_origin,2,3,1,1);
+ Dt=real(ifft(MR));                             %transform the data back to time domain
+ snr(a)= SNR1(seis_origin(:),Dt(:))             %compute SNR
+ Threedimension(seis_origin,2,3,1,1);           %plot data
 Threedimension(seis_sam,2,3,1,2);
     
 Threedimension(Dt,2,3,1,3);
